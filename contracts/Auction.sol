63b425fc557f508b6bf39eb43065ea0a07b62b5f
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-11-27 15:20:06 -0500

    feat: initial scaffolding

---
+++ contracts/Auction.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Auction {
+    
+
+
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,6 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-import "./tasks/accounts";
-import "./tasks/clean";
-import "./tasks/deploy";
 
 import { resolve } from "path";
 
@@ -75,7 +72,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.7",
+    version: "0.8.0",
     settings: {
       metadata: {
         // Not including the metadata hash
@@ -86,7 +83,7 @@ const config: HardhatUserConfig = {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
         enabled: true,
-        runs: 800,
+        runs: 200,
       },
     },
   },
